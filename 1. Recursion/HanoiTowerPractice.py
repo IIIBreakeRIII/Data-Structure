d3374c9b0d46b0b7da3262e5e06e68055a0e258b
IIIBreakeRIII <89850286+IIIBreakeRIII@users.noreply.github.com>
2023-01-12 20:50:45 +0900

    Recursion Finish

--- 1. Recursion/HanoiTowerPractice.py
+++ 1. Recursion/HanoiTowerPractice.py
@@ -5,15 +5,21 @@
 
 def MoveDisk(DiskNum, StartPeg, EndPeg):
   print("%d번 원판을 %d번 기둥에서 %d번 기둥으로 이동" % (DiskNum, StartPeg, EndPeg))
+  print("움직이는 조건 수행 출력")
 
 def Hanoi(DiskNum, StartPeg, EndPeg):
   if DiskNum == 0:
+    print("조건 수행 완료")
     return
   else:
-    OtherPeg = 6 - StartPeg - EndPeg
+    OtherPeg = 6 - StartPeg - EndPeg            # 2
     Hanoi(DiskNum - 1, StartPeg, OtherPeg)
+    print("조건 1 수행")
     MoveDisk(DiskNum, StartPeg, EndPeg)
+    print("움직이는 조건 수행")
     Hanoi(DiskNum - 1, OtherPeg, EndPeg)
+    print("조건 2 수행")
+
 
 # Test Code
 
