be75887723d732d5e54adc12e24168d093ac827f
IIIBreakeRIII <89850286+IIIBreakeRIII@users.noreply.github.com>
2023-01-12 19:10:38 +0900

    Recursion Practice

---
+++ 1. Recursion/HanoiTower.py
@@ -0,0 +1,21 @@
+# 하노이의 탑 문제
+# 진짜 이해하고 싶다
+
+# 코드잇 답
+
+def MoveDisk(DiskNum, StartPeg, EndPeg):
+  print("%d번 원판을 %d번 기둥에서 %d번 기둥으로 이동" % (DiskNum, StartPeg, EndPeg))
+
+def Hanoi(DiskNum, StartPeg, EndPeg):
+  if DiskNum == 0:
+    return
+  else:
+    OtherPeg = 6 - StartPeg - EndPeg
+    Hanoi(DiskNum - 1, StartPeg, OtherPeg)
+    MoveDisk(DiskNum, StartPeg, EndPeg)
+    Hanoi(DiskNum - 1, OtherPeg, EndPeg)
+
+
+# Test Code
+
+Hanoi(3, 1, 3)
