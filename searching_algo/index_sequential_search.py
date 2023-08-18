323943b2f7a2376272a1bbf2f4ea78aa8b7eedca
IIIBreakeRIII <ryu990305@icloud.com>
2023-08-18 20:19:08 +0900

    index_seq_search

--- searching_algo/index_sequential_search.py
+++ searching_algo/index_sequential_search.py
@@ -1,10 +1,10 @@
 # Index Sequential Search
 # 색인 순차 탐색
 
-index_size = 3
-
 num_list = [3, 9, 15, 22, 31, 55, 67, 88, 91]
 index_table = [[0, 3], [2, 15], [6, 67]]
+
+index_size = len(index_table)
 
 def sequential_search(key, low, high):
     for i in range(low, high, 1):
@@ -21,13 +21,17 @@ def index_search(key):
     if key < num_list[0] or key > num_list[len(num_list) - 1]:
         return -1
     
-    for i in range(0, index_size - 1, 1):
-        if index_table[i][0] <= key and index_table[i + 1][0] > key:
-            break
+    for i in range(0, index_size, 1):
+        if i < index_size - 1:
+            if index_table[i][1] <= key and index_table[i + 1][1] > key:
+                break
+        elif i == index_size - 1:
+            if index_table[i][1] >= key:
+                break
 
-    if i == index_size:
+    if i == index_size - 1:
         low = index_table[i - 1][0]
-        high = index_size - 1
+        high = len(num_list)
 
     else:
         low = index_table[i][0]
@@ -36,4 +40,4 @@ def index_search(key):
     return sequential_search(key, low, high)
 
 
-index_search(88)
+index_search(31)
